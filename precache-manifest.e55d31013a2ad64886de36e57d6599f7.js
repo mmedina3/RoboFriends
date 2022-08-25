1b736113493a1c3fdee987b8494a5474fd26d6aa
Michelle Medina <michelle.medina@mixpanel.com>
2022-08-25 14:55:58 -0700

    Updates

---
+++ precache-manifest.e55d31013a2ad64886de36e57d6599f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4952eb59a85c962eef88",
+    "url": "/RoboFriends/static/css/main.7b6acde2.chunk.css"
+  },
+  {
+    "revision": "4952eb59a85c962eef88",
+    "url": "/RoboFriends/static/js/main.4952eb59.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RoboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "66ec5a627fcee24a9dda",
+    "url": "/RoboFriends/static/css/2.35bfa3d2.chunk.css"
+  },
+  {
+    "revision": "66ec5a627fcee24a9dda",
+    "url": "/RoboFriends/static/js/2.66ec5a62.chunk.js"
+  },
+  {
+    "revision": "4cb7496509aa3077e2b1",
+    "url": "/RoboFriends/static/js/runtime~main.4cb74965.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ce8aab18c28488623beb02e8304da1ee",
+    "url": "/RoboFriends/index.html"
+  }
+];
